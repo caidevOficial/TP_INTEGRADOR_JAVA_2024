16cf9587c9a9684508ba14dec01dfe85f3707a95
CaidevOficial <ffalcone@fra.utn.edu.ar>
2024-07-07 17:12:51 -0300

    ♻️ Update AdminInformes Servlet

--- src/servlets/servletAdminInformes.java
+++ src/servlets/servletAdminInformes.java
@@ -41,8 +41,7 @@ public class servletAdminInformes extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 	
 	protected void cargarInformes(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
